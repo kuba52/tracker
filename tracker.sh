be9128d154f1e0b71ce93954f86cae5fe9b3ea0d
jacek <jacek.muszynski.mail@gmail.com>
2023-06-12 15:17:38 +0200

    script

--- tracker.sh
+++ tracker.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-python3 detect.py
-./cam.wls
-python3 IO_wykresy.py
+g++ -std=c++20 main.cc -o p
+if [ $# -le 0 ]
+then
+    echo "usage $0 [distance to plane]"
+else
+    echo $#
+    python3 detect.py &&
+    ./p 1280 720 $1 &&
+    python3 IO_wykresy.py
+fi
